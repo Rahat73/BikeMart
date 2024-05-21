ef8172422546036076407724f6e577d0eb96acd3
Rahat73 <rahat.ashik.18@gmail.com>
2024-05-22 01:24:18 +0600

    first commit

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import cors from 'cors';
+import express, { Application, Request, Response } from 'express';
+const app: Application = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use('/api/v1/students');
+
+app.get('/test', (req: Request, res: Response) => {
+  const test = 'Hello World!';
+  res.send(test);
+});
+
+export default app;
